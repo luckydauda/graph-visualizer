1030133694f42234dbfb1286b2d3082449facf66
luckydauda <xanderlawki@outlook.com>
2025-01-24 06:35:11 +0100

    graph ui components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,10 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -22,12 +26,12 @@ const config = {
         "fixStyle": "inline-type-imports"
       }
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
+    // "@typescript-eslint/no-unused-vars": [
+    //   "warn",
+    //   {
+    //     "argsIgnorePattern": "^_"
+    //   }
+    // ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
